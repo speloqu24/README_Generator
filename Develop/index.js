a1d56b67fa520f90a2355ef413837d272bd7e65c
Speloquin <speloqu24@gmail.com>
2020-11-17 22:12:35 -0700

    commit end of night

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,14 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
+const generateMarkdown = require("./generateMarkdown.js");
 
-const writeToFile = util.promisify(fs.writeFile);
+// // const writeToFile = util.promisify(fs.writeFile);
 
 // array of questions for user
 const questions = [
   {
     type: "input",
-    name: "project_title",
+    name: "title",
     message: "What is the title of your project?",
   },
   {
@@ -41,9 +41,6 @@ function writeToFile(fileName, data) {
     error ? console.error(error) : console.log(data)
   );
 }
-
-// // dynamically create the README
-// const generateReadMe = (response) =>
 
 // function to initialize program - kicks off questions
 function init() {
